8e56a6cd1186d26bc64a10971791f035729de4c7
Guus Kuiper <guus.kuiper@demcon.nl>
2019-12-12 15:24:16 +0100

    Day 1 and 2

---
+++ Day1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = File.OpenRead("input.txt");
+            var sum = 0;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var res = ProcessLine(line);
+                    var cov = Convert(res);
+                    sum += cov;
+                }
+            }
+
+            Console.WriteLine($"Sum = {sum}");
+            Debug.Assert(sum == 3375962);
+
+            Console.WriteLine("1Part2");
+            var fuelForFuel = 0;
+            file = File.OpenRead("input.txt");
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var res = ProcessLine(line);
+                    var cov = RecursiveFuel(res);
+                    fuelForFuel += cov;
+                }
+            }
+
+            Console.WriteLine($"Sum = {fuelForFuel}");
+            Debug.Assert(fuelForFuel == 5061072);
+        }
+
+        private static int ProcessLine(string line)
+        {
+            return int.Parse(line);
+        }
+
+        private static int Convert(int mass)
+        {
+            var fuel = (int)Math.Floor((double)mass / 3) - 2;
+            return fuel;
+        }
+
+        private static int RecursiveFuel(int fuel)
+        {
+            var additionalFuel = Convert(fuel);
+            if (additionalFuel >= 0)
+            {
+                additionalFuel += RecursiveFuel(additionalFuel);
+            }
+            else
+            {
+                return 0;
+            }
+            return additionalFuel;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("input.txt");
+            //string text = "1,9,10,3,2,3,11,0,99,30,40,50";
+            var instructions = text.Split(',').Select(int.Parse).ToArray();
+
+            //// change input
+            instructions[1] = 12;
+            instructions[2] = 2;
+
+            var elfComputer = new ElfComputer(instructions);
+            elfComputer.ProcessInstructions();
+
+            Debug.Assert(elfComputer.GetData(0) == 12490719);
+
+            for (int noun = 0; noun <= 99; noun++)
+            {
+                for (int verb = 0; verb <= 99; verb++)
+                {
+                    instructions[1] = noun;
+                    instructions[2] = verb;
+
+                    elfComputer = new ElfComputer(instructions);
+                    elfComputer.ProcessInstructions();
+
+                    if (elfComputer.GetData(0) == 19690720)
+                    {
+                        Console.WriteLine($"Noun {noun}, verb {verb}");
+                        return;
+                    }
+                }
+            }
+
+            Console.WriteLine("No solution found");
+        }
+    }
+}

2d1dd6ebac6f4da544c9df69da5a4073e05b0213
Guus Kuiper <guuskuiper@gmail.com>
2019-12-30 20:02:05 +0100

    Day11

---
+++ Day11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Day5;
+
+namespace Day11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("input.txt");
+            var instructions = text.Split(',').Select(long.Parse).ToArray();
+
+            var robot = new Robot(instructions);
+            var painted = robot.StartRobot();
+
+            System.Console.WriteLine("Painted " + painted);
+        }
+    }
+}

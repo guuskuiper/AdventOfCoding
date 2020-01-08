eab3011cad8f9a5c1d52ddcac814b08541cb4003
Guus Kuiper <guuskuiper@gmail.com>
2020-01-08 21:02:55 +0100

    Day17 Part2

--- Day17/ASCII.cs
+++ Day17/ASCII.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Text;
 using Day5;
 
 namespace Day17
@@ -27,7 +28,13 @@ namespace Day17
 
         private int robotX;
         private int robotY;
+        private int endX;
+        private int endY;
         private Direction robotDirection;
+        private string inputData;
+        private int inputCount;
+
+        bool mode;
 
         public ASCII(IEnumerable<long> instructions)
         {
@@ -41,63 +48,88 @@ namespace Day17
             computer.ProcessInstructions();
 
             FindCrossings();
+            FindEnd();
             StraightPath();
+        }
+
+        public void Start2(string inputs)
+        {
+            mode = true;
+            inputData = inputs;
+            inputCount = 0;
+
+            computer.SetData(0, 2);
+
+            computer.ProcessInstructions();
         }
 
         public long Input()
         {
-            return 0;
+            var ch = inputData[inputCount];
+            System.Console.Write(ch);
+            inputCount++;
+            return Convert.ToInt32(ch);
         }
 
         public void Output(long output)
         {
-            var ch = Convert.ToChar(output);
-            Console.Write(ch);
-            switch(ch)
+            if(output > 256)
             {
-                case '>':
-                    robotX = readX;
-                    robotY = readY;
-                    robotDirection = Direction.Right;
-                    grid[readX, readY] = true;
-                    readX++;
-                    break;
-                case '<':
-                    robotX = readX;
-                    robotY = readY;
-                    robotDirection = Direction.Left;
-                    grid[readX, readY] = true;
-                    readX++;
-                    break;
-                case 'v':
-                    robotX = readX;
-                    robotY = readY;
-                    robotDirection = Direction.Down;
-                    grid[readX, readY] = true;
-                    readX++;
-                    break;
-                case '^':
-                    robotX = readX;
-                    robotY = readY;
-                    robotDirection = Direction.Up;
-                    grid[readX, readY] = true;
-                    readX++;
-                    break;
-                case '#':
-                    grid[readX, readY] = true;
-                    readX++;
-                    break;
-                case '.':
-                    grid[readX, readY] = false;
-                    readX++;
-                    break;
-                case '\n':
-                    readX = 0;
-                    readY++;
-                    break;
-                default:
-                    throw new Exception($"Unknown character {output}-{ch}");
-                    break;
+                System.Console.WriteLine("Output: " + output);
+                return;
+            }
+            var ch = Convert.ToChar(output);
+
+            Console.Write(ch);
+            if(!mode)
+            {
+                switch(ch)
+                {
+                    case '>':
+                        robotX = readX;
+                        robotY = readY;
+                        robotDirection = Direction.Right;
+                        grid[readX, readY] = true;
+                        readX++;
+                        break;
+                    case '<':
+                        robotX = readX;
+                        robotY = readY;
+                        robotDirection = Direction.Left;
+                        grid[readX, readY] = true;
+                        readX++;
+                        break;
+                    case 'v':
+                        robotX = readX;
+                        robotY = readY;
+                        robotDirection = Direction.Down;
+                        grid[readX, readY] = true;
+                        readX++;
+                        break;
+                    case '^':
+                        robotX = readX;
+                        robotY = readY;
+                        robotDirection = Direction.Up;
+                        grid[readX, readY] = true;
+                        readX++;
+                        break;
+                    case '#':
+                        grid[readX, readY] = true;
+                        readX++;
+                        break;
+                    case '.':
+                        grid[readX, readY] = false;
+                        readX++;
+                        break;
+                    case '\n':
+                        readX = 0;
+                        readY++;
+                        break;
+                    default:
+                        System.Console.WriteLine($"Unknown character {output}({ch})");
+                        //throw new Exception($"Unknown character {output}-{ch}");
+                        break;
+                }
             }
         }
 
@@ -124,18 +156,58 @@ namespace Day17
             return sum;
         }
 
+        private void FindEnd()
+        {
+            for(int y = 1; y < HEIGHT - 1; y++)
+            {
+                for(int x = 1; x < WIDTH - 1; x++)
+                {
+                    var nearCount = 0;
+                    if(!grid[x,y]) continue;
+                    if(grid[x+1,y]) nearCount++;
+                    if(grid[x-1,y]) nearCount++;
+                    if(grid[x,y+1]) nearCount++;
+                    if(grid[x,y-1]) nearCount++;
+
+                    if(nearCount == 1 && x != robotX && y != robotY)
+                    {
+                        endX = x;
+                        endY = y;
+                        System.Console.WriteLine($"End at {x},{y}");
+                        //return;
+                    }
+                    
+                }
+            }
+        }
+
+// AAAAAAAAAAAAAAAA BBBBBBBBBBB AAAAAAAAAAAAAAAA CCCCCCCCCCCCCCCCC AAAAAAAAAAAAAAAA BBBBBBBBBBB CCCCCCCCCCCCCCCCC BBBBBBBBBBB CCCCCCCCCCCCCCCCC BBBBBBBBBBB
+// L,10,R,8,L,6,R,6,L,8,L,8,R,8,L,10,R,8,L,6,R,6,R,8,L,6,L,10,L,10,L,10,R,8,L,6,R,6,L,8,L,8,R,8,R,8,L,6,L,10,L,10,L,8,L,8,R,8,R,8,L,6,L,10,L,10,L,8,L,8,R,8
+
+// A: 
+// L,10,R,8,L,6,R,6
+
+// B:
+// L,8,L,8,R,8
+
+// C:
+// R8,L,6,L,10,L,10
+
+// A,B,A,C,A,B,C,B,C,B
+
         private void StraightPath()
         {
             int curX = robotX;
             int curY = robotY;
             Direction curDir = robotDirection;
 
+
             int tempX = 0;
             int tempY = 0;
 
             int steps = 0;
-            string commands = "";
-            while(true)
+            var sb = new StringBuilder("");
+            while(! (curX == endX && curY == endY))
             {
                 if(DirectionPosition(curX, curY, curDir, out tempX, out tempY))
                 {
@@ -146,25 +218,29 @@ namespace Day17
                 else
                 {
                     // find new direction
-                    commands += steps;
+                    if(steps > 0)
+                    {
+                        sb.Append(steps);
+                        sb.Append(',');
+                    }
                     steps = 0;
                     bool turn = false; // false = left, true = right
-                    if(DirectionPosition(curX, curY, Direction.Up, out tempX, out tempY))
+                    if(curDir != Direction.Down && DirectionPosition(curX, curY, Direction.Up, out tempX, out tempY))
                     {
                         turn = curDir == Direction.Left;
                         curDir = Direction.Up;
                     }
-                    else if(DirectionPosition(curX, curY, Direction.Left, out tempX, out tempY))
+                    else if(curDir != Direction.Right && DirectionPosition(curX, curY, Direction.Left, out tempX, out tempY))
                     {
                         turn = curDir == Direction.Down;
                         curDir = Direction.Left;
                     }
-                    else if(DirectionPosition(curX, curY, Direction.Down, out tempX, out tempY))
+                    else if(curDir != Direction.Up && DirectionPosition(curX, curY, Direction.Down, out tempX, out tempY))
                     {
                         turn = curDir == Direction.Right;
                         curDir = Direction.Down;
                     }
-                    else if(DirectionPosition(curX, curY, Direction.Right, out tempX, out tempY))
+                    else if(curDir != Direction.Left && DirectionPosition(curX, curY, Direction.Right, out tempX, out tempY))
                     {
                         turn = curDir == Direction.Up;
                         curDir = Direction.Right;
@@ -173,32 +249,41 @@ namespace Day17
                     {
                         throw new Exception("Crashed!!!");
                     }
-                    commands.Append(turn ? 'R' : 'L');
+                    char dirChat = turn ? 'R' : 'L';
+                    sb.Append(dirChat);
+                    sb.Append(',');
                 }
-                System.Console.WriteLine(commands);
             }
+
+            if(steps > 0)
+            {
+                sb.Append(steps);
+                sb.Append(',');
+            }
+
+            System.Console.WriteLine("Final: " + sb);
         }
 
         private bool DirectionPosition(int x, int y, Direction directory, out int x2, out int y2)
         {
             switch(directory)
             {
-                case Direction.Up:
+                case Direction.Left:
                     x2 = x - 1;
                     y2 = y;
                     if(x2 < 0) return false;
                     break;
-                case Direction.Down:
+                case Direction.Right:
                     x2 = x + 1;
                     y2 = y;
                     if(x2 >= WIDTH) return false;
                     break;
-                case Direction.Left:
+                case Direction.Up:
                     x2 = x;
                     y2 = y - 1;
                     if(y2 < 0) return false;
                     break;
-                case Direction.Right:
+                case Direction.Down:
                     x2 = x;
                     y2 = y + 1;
                     if(y2 >= HEIGHT) return false;
--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Day17
 {
@@ -13,6 +14,26 @@ namespace Day17
 
             var ascii = new ASCII(instructions);
             ascii.Start();
+
+            // manually splitting it up into 3 parts (A, B, C) and creating the main sequence:
+            // A,B,A,C,A,B,C,B,C,B
+            // A: 
+            // L,10,R,8,L,6,R,6
+
+            // B:
+            // L,8,L,8,R,8
+
+            // C:
+            // R,8,L,6,L,10,L,10
+            var sb = new StringBuilder();
+            sb.AppendLine("A,B,A,C,A,B,C,B,C,B"); // main
+            sb.AppendLine("L,10,R,8,L,6,R,6"); // A
+            sb.AppendLine("L,8,L,8,R,8"); // B
+            sb.AppendLine("R,8,L,6,L,10,L,10"); // C
+            sb.AppendLine("n");// continues feed  y/ n
+
+            var ascii2 = new ASCII(instructions);
+            ascii2.Start2(sb.ToString());
         }
     }
 }

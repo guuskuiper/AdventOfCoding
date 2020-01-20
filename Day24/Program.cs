08cb77076bc8ca6f0526387e48b4f8f8b2e35f68
Guus Kuiper <guus.kuiper@demcon.nl>
2020-01-20 14:36:05 +0100

    Day24 Part2

--- Day24/Program.cs
+++ Day24/Program.cs
@@ -12,6 +12,13 @@ namespace Day24
 
             var gameOfLife = new GameOfLife(text);
             gameOfLife.CalcSame();
+
+            var example = File.ReadAllLines("example.txt");
+            var gameOfLifeExample = new GameOfLife(example);
+            gameOfLifeExample.Steps(10);
+
+            var gameOfLife2 = new GameOfLife(text);
+            gameOfLife2.Steps(200);
         }
     }
 }

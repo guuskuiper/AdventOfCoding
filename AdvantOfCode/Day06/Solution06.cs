e86bc3d6a9fc9f356c7c6da9de6878bafc24ea84
Guus Kuiper <guuskuiper@gmail.com>
2021-12-06 07:49:48 +0100

    Day06

--- AdvantOfCode/Day06/Solution06.cs
+++ AdvantOfCode/Day06/Solution06.cs
@@ -2,8 +2,87 @@ namespace AdventOfCode.Day06;
 
 public class Solution06 : Solution
 {
+    private List<int> simulation;
+    private long[] bins;
     public string Run()
     {
-        return "UNKNOWN";
+        string input = InputReader.ReadFile();
+
+        simulation = input.Split(',').Where(x => !string.IsNullOrEmpty(x)).Select(int.Parse).ToList();
+        bins = Bins(simulation);
+
+        SimulateDays(80, Simulate);
+        int A = simulation.Count;
+
+        SimulateDays(256, SimulateBin);
+        long B = CountBins();
+        
+        return A  + " \n" + B;
+    }
+
+
+    private void SimulateDays(int days, Action simulate)
+    {
+        for (int i = 0; i < days; i++)
+        {
+            simulate();
+        }
+    }
+    
+    private void Simulate()
+    {
+        for (int i = simulation.Count - 1; i >= 0; i--)
+        {
+            if (simulation[i] == 0)
+            {
+                simulation[i] = 6;
+                simulation.Add(8);
+            }
+            else
+            {
+                simulation[i]--;
+            }
+        }
+    }
+    
+    private void SimulateBin()
+    {
+        long[] newBins = new long[9];
+        for (int i = 0; i < 9; i++)
+        {
+            if (i == 0)
+            {
+                newBins[8] += bins[0];
+                newBins[6] += bins[0];
+            }
+            else
+            {
+                newBins[i - 1] += bins[i];
+            }
+        }
+        
+        bins = newBins;
+    }
+
+    private long[] Bins(List<int> numbers)
+    {
+        long[] bins = new long[9];
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            bins[numbers[i]]++;
+        }
+
+        return bins;
+    }
+    
+    private long CountBins()
+    {
+        long count = 0;
+        for (int i = 0; i < bins.Length; i++)
+        {
+            count += bins[i];
+        }
+
+        return count;
     }
 }

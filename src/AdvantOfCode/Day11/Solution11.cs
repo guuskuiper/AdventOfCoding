911ece573d439c90990e02a8eefeef1471650f65
Guus Kuiper <guuskuiper@gmail.com>
2021-12-11 08:56:02 +0200

    Refactor11

--- src/AdvantOfCode/Day11/Solution11.cs
+++ src/AdvantOfCode/Day11/Solution11.cs
@@ -5,7 +5,7 @@ public class Solution11 : Solution
     public class Cave
     {
         public const int SIZE = 10;
-        public Tile[,] Grid = new Tile[SIZE, SIZE];
+        public readonly Tile[,] Grid = new Tile[SIZE, SIZE];
     }
     
     public class Tile
@@ -17,17 +17,19 @@ public class Solution11 : Solution
     private Cave _cave;
     private long _flashes = 0;
     private long _steps = 0;
-    
+
     public string Run()
     {
         var lines = InputReader.ReadFileLines();
-        
         Parse(lines);
+        
         Steps(100);
         long A = _flashes;
+
         RunUntil();
+        long B = _steps;
         
-        return A + "\n" + _steps;
+        return A + "\n" + B;
     }
 
     private void Steps(int number)
@@ -54,50 +56,63 @@ public class Solution11 : Solution
 
     private bool Step()
     {
-        for (int i = 0; i < Cave.SIZE; i++)
-        {
-            for (int j = 0; j < Cave.SIZE; j++)
-            {
-                _cave.Grid[i, j].Energy += 1;
-            }
-        }
+        ApplyToGrid(Increase);
         
-        for (int i = 0; i < Cave.SIZE; i++)
-        {
-            for (int j = 0; j < Cave.SIZE; j++)
-            {
-                if (_cave.Grid[i, j].Energy > 9)
-                {
-                    Flash(i, j);
-                }
-            }
-        }
+        ApplyToGrid(CheckFlash);
 
         bool allFlashed = true;
-        for (int i = 0; i < Cave.SIZE; i++)
-        {
-            for (int j = 0; j < Cave.SIZE; j++)
-            {
-                if (!_cave.Grid[i, j].Flashed)
-                {
-                    allFlashed = false;
-                }
-            }
-        }
-        
-        for (int i = 0; i < Cave.SIZE; i++)
-        {
-            for (int j = 0; j < Cave.SIZE; j++)
-            {
-                if (_cave.Grid[i, j].Flashed)
-                {
-                    _cave.Grid[i, j].Energy = 0;
-                    _cave.Grid[i, j].Flashed = false;
-                }
-            }
-        }
+        ApplyToGrid(IsSynchronized);
+
+        ApplyToGrid(ResetFlashed);
 
         return allFlashed;
+        
+        void IsSynchronized(Tile tile)
+        {
+            if (!tile.Flashed)
+            {
+                allFlashed = false;
+            }
+        }
+    }
+
+    private void Increase(Tile t)
+    {
+        t.Energy += 1;
+    }
+
+    private void CheckFlash(Tile tile, int i, int j)
+    {
+        if (tile.Energy > 9)
+        {
+            Flash(i, j);
+        }
+    }
+    
+    private void ResetFlashed(Tile tile)
+    {
+        if (tile.Flashed)
+        {
+            tile.Energy = 0;
+            tile.Flashed = false;
+        }
+    }
+
+    private void ApplyToGrid(Action<Tile> action)
+    {
+        void ActionWithoutPosition(Tile tile, int i, int j) => action(tile);
+        ApplyToGrid(ActionWithoutPosition);
+    }
+    
+    private void ApplyToGrid(Action<Tile, int, int> action)
+    {
+        for (int i = 0; i < Cave.SIZE; i++)
+        {
+            for (int j = 0; j < Cave.SIZE; j++)
+            {
+                action(_cave.Grid[i, j], i, j);
+            }
+        }
     }
 
     private void Flash(int i, int j)
@@ -115,21 +130,13 @@ public class Solution11 : Solution
                 int idi = i + di;
                 int jdj = j + dj;
                 
-                Increase(idi, jdj);
-                
-                if (OnGrid(idi, jdj) && _cave.Grid[idi, jdj].Energy > 9)
-                {
-                    Flash(idi, jdj);
-                }
+                if(!OnGrid(idi, jdj)) continue;
+
+                var tile = _cave.Grid[idi, jdj];
+                Increase(tile);
+                CheckFlash(tile, idi, jdj);
             }
         }
-    }
-
-    private void Increase(int i, int j)
-    {
-        if(!OnGrid(i, j)) return;
-        
-        _cave.Grid[i, j].Energy += 1;
     }
 
     private bool OnGrid(int i, int j)

46f145f2d7f03ae31aeff84628016697417e1c60
guuskuiper <guuskuiper@gmail.com>
2021-12-13 15:17:09 +0100

    Use CallerName for unit tests

--- test/AdventOfCode.Test/DaySolutionTest.cs
+++ test/AdventOfCode.Test/DaySolutionTest.cs
@@ -1,23 +1,26 @@
 using System;
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace AdventOfCode.Test;
 
 public class DaySolutionTest
 {
-    [Fact] public void Day01() => AssertDay(1, "1548", "1589");
-    [Fact] public void Day02() => AssertDay(2, "1746616", "1741971043");
-    [Fact] public void Day03() => AssertDay(3, "4138664", "4273224");
-    [Fact] public void Day04() => AssertDay(4, "31424", "23042");
-    [Fact] public void Day05() => AssertDay(5, "6666", "19081");
-    [Fact] public void Day06() => AssertDay(6, "360268", "1632146183902");
-    [Fact] public void Day07() => AssertDay(7, "323647", "87640209");
-    [Fact] public void Day08() => AssertDay(8, "476", "1011823");
-    [Fact] public void Day09() => AssertDay(9, "500", "970200");
-    [Fact] public void Day10() => AssertDay(10, "243939", "2421222841");
-    [Fact] public void Day11() => AssertDay(11, "1675", "515");
-    [Fact] public void Day12() => AssertDay(12, "5254", "149385");
-    [Fact] public void Day13() => AssertDay(13, "684", "" +
+    private const string Prefix = "Day";
+    
+    [Fact] public void Day01() => AssertDay("1548", "1589");
+    [Fact] public void Day02() => AssertDay("1746616", "1741971043");
+    [Fact] public void Day03() => AssertDay("4138664", "4273224");
+    [Fact] public void Day04() => AssertDay("31424", "23042");
+    [Fact] public void Day05() => AssertDay("6666", "19081");
+    [Fact] public void Day06() => AssertDay("360268", "1632146183902");
+    [Fact] public void Day07() => AssertDay("323647", "87640209");
+    [Fact] public void Day08() => AssertDay("476", "1011823");
+    [Fact] public void Day09() => AssertDay("500", "970200");
+    [Fact] public void Day10() => AssertDay("243939", "2421222841");
+    [Fact] public void Day11() => AssertDay("1675", "515");
+    [Fact] public void Day12() => AssertDay("5254", "149385");
+    [Fact] public void Day13() => AssertDay("684", "" +
 "  ## ###  #### ###  #     ##  #  # #  # " + Environment.NewLine +
 "   # #  #    # #  # #    #  # # #  #  # " + Environment.NewLine +
 "   # #  #   #  ###  #    #    ##   #### " + Environment.NewLine +
@@ -25,8 +28,9 @@ public class DaySolutionTest
 "#  # # #  #    #  # #    #  # # #  #  # " + Environment.NewLine +
 " ##  #  # #### ###  ####  ### #  # #  # " + Environment.NewLine);
 
-    private void AssertDay(int day, string expectedA, string expectedB)
+    private void AssertDay(string expectedA, string expectedB, [CallerMemberName] string callerName = "")
     {
+        int day = int.Parse(callerName.Substring(Prefix.Length));
         string solutionName = $"Solution{day:D2}";
         Solution current = DayGenerator.GetByName(solutionName);
         var solution = current.Run();

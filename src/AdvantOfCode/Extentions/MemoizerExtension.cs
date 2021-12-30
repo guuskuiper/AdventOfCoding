fee851126bd104acb35c86b8aaf5d984820e89b3
guuskuiper <guuskuiper@gmail.com>
2021-12-30 13:00:06 +0100

    Memoizer

---
+++ src/AdvantOfCode/Extentions/MemoizerExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+
+namespace AdventOfCode.Extentions;
+
+public static class MemoizerExtension
+{
+    public static Func<TRes> Memoize<T1, TRes>(this Func<TRes> f) where TRes : class
+    {
+        TRes? cache = null;
+
+        return () =>
+        {
+            if (cache is null)
+            {
+                cache = f();
+            }
+
+            return cache;
+        };
+    }
+    
+    public static Func<T1, TRes> Memoize<T1, TRes>(this Func<T1, TRes> f)
+    {
+        ConcurrentDictionary<T1, TRes> cache = new();
+        return args1 => cache.GetOrAdd(args1, f(args1));
+    }
+    
+    public static Func<T1, T2, TRes> Memoize<T1, T2, TRes>(this Func<T1, T2, TRes> f)
+    {
+        ConcurrentDictionary<(T1, T2), TRes> cache = new();
+        return (args1, args2) => cache.GetOrAdd((args1, args2), f(args1, args2));
+    }
+    
+    public static Func<T1, T2, T3, TRes> Memoize<T1, T2, T3, TRes>(this Func<T1, T2, T3, TRes> f)
+    {
+        ConcurrentDictionary<(T1, T2, T3), TRes> cache = new();
+        return (args1, args2, args3) => cache.GetOrAdd((args1, args2, args3), f(args1, args2, args3));
+    }
+}
---
+++ test/AdventOfCode.Test/MemoizerExtensionsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using AdventOfCode.Extentions;
+using Xunit;
+
+namespace AdventOfCode.Test;
+
+public class MemoizerExtensionsTest
+{
+    [Fact]
+    public void Fib3_ShouldReturn2()
+    {
+        int fib = Fib(3);
+        Assert.Equal(2, fib);
+    }
+    
+    [Fact]
+    public void Fib4_ShouldReturn3()
+    {
+        int fib = Fib(40);
+        Assert.Equal(3, fib);
+    }
+    
+    [Fact]
+    public void Fib3Memoizer_ShouldReturn2()
+    {
+        Func<int, int> fib = null;
+        fib = MemoizerExtension.Memoize<int, int>(n => Fib(n, fib!));
+
+        int fibResult = fib(3);
+        
+        Assert.Equal(2, fibResult);
+    }
+    
+    [Fact]
+    public void Fib4Memoizer_ShouldReturn3()
+    {
+        Func<int, int> fib = null;
+        fib = MemoizerExtension.Memoize<int, int>(n => Fib(n, fib!));
+
+        int fibResult = fib(40);
+        
+        Assert.Equal(3, fibResult);
+    }
+
+    private int Fib(int n) => Fib(n, Fib);
+
+    private int Fib(int n, Func<int, int> fib) => n > 2 ? fib(n - 1) + fib(n - 2) : 1;
+}

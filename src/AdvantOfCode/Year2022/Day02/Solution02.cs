ddfa749d9bb95cca723f57705974c3bbc60db96a
Guus Kuiper <guus.kuiper@demcon.com>
2022-12-02 15:53:34 +0100

    Cleanup Day02

--- src/AdvantOfCode/Year2022/Day02/Solution02.cs
+++ src/AdvantOfCode/Year2022/Day02/Solution02.cs
@@ -2,40 +2,58 @@ using System.Collections.Generic;
 
 namespace AdventOfCode.Year2022.Day02;
 
+public enum ERockPaperScissors
+{
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+};
+
+public static class ERockPaperScissorsExtentions
+{
+    public static ERockPaperScissors Win(this ERockPaperScissors shape) => shape switch
+    {
+        ERockPaperScissors.Rock => ERockPaperScissors.Paper,
+        ERockPaperScissors.Paper => ERockPaperScissors.Scissors,
+        ERockPaperScissors.Scissors => ERockPaperScissors.Rock,
+        _ => throw new ArgumentOutOfRangeException(nameof(shape), shape, null)
+    };
+
+    public static ERockPaperScissors Lose(this ERockPaperScissors shape) => shape switch
+    {
+        ERockPaperScissors.Rock => ERockPaperScissors.Scissors,
+        ERockPaperScissors.Paper => ERockPaperScissors.Rock,
+        ERockPaperScissors.Scissors => ERockPaperScissors.Paper,
+        _ => throw new ArgumentOutOfRangeException(nameof(shape), shape, null)
+    };
+}
+
 public class Solution02 : Solution
 {
     private record Round(ERockPaperScissors Opponent, ERockPaperScissors Response);
-    private enum ERockPaperScissors
-    {
-        Rock,
-        Paper,
-        Scissors,
-    };
 
     public string Run()
     {
         List<string> lines = InputReader.ReadFileLines();
-        List<Round> rounds = Parse(lines);
+        IEnumerable<Round> rounds = Parse(lines);
 
         long score = Scores(rounds);
 
-        List<Round> changedRounds = ChangeRounds(rounds);
+        IEnumerable<Round> changedRounds = ChangeRounds(rounds);
         long scoresChanged = Scores(changedRounds);
 
         return score + "\n" + scoresChanged;
     }
 
-    private List<Round> Parse(IEnumerable<string> lines)
+    private IEnumerable<Round> Parse(IEnumerable<string> lines)
     {
-        List<Round> rounds = new List<Round>();
         foreach (var line in lines)
         {
             string[] split = line.Split(' ');
             ERockPaperScissors opponent = Parse(split[0]);
             ERockPaperScissors response = Parse(split[1]);
-            rounds.Add(new Round(opponent, response));
+            yield return new Round(opponent, response);
         }
-        return rounds;
     }
 
     private long Scores(IEnumerable<Round> rounds)
@@ -51,20 +69,14 @@ public class Solution02 : Solution
 
     private long Score(Round round)
     {
-        long shapeScore = round.Response switch
-        {
-            ERockPaperScissors.Rock => 1,
-            ERockPaperScissors.Paper => 2,
-            ERockPaperScissors.Scissors => 3,
-            _ => throw new ArgumentOutOfRangeException()
-        };
+        long shapeScore = (int)round.Response;
 
         long outcome;
         if (round.Response == round.Opponent)
         {
             outcome = 3;
         }
-        else if(round.Response == Win(round.Opponent))
+        else if(round.Response == round.Opponent.Win())
         {
             outcome = 6;
         }
@@ -76,15 +88,13 @@ public class Solution02 : Solution
         return shapeScore + outcome;
     }
 
-    private List<Round> ChangeRounds(List<Round> rounds)
+    private IEnumerable<Round> ChangeRounds(IEnumerable<Round> rounds)
     {
-        List<Round> newRounds = new List<Round>();
         foreach (var round in rounds)
         {
             ERockPaperScissors response = ChangeRound(round);
-            newRounds.Add(round with { Response = response });
+            yield return round with { Response = response };
         }
-        return newRounds;
     }
 
     private ERockPaperScissors ChangeRound(Round round)
@@ -92,42 +102,23 @@ public class Solution02 : Solution
         ERockPaperScissors newResponse = round.Response switch
         {
             //X means you need to lose,
-            ERockPaperScissors.Rock => Lose(round.Opponent),
+            ERockPaperScissors.Rock => round.Opponent.Lose(),
             //Y means you need to end the round in a draw, and
             ERockPaperScissors.Paper => round.Opponent,
             //Z means you need to win."
-            ERockPaperScissors.Scissors => Win(round.Opponent),
+            ERockPaperScissors.Scissors => round.Opponent.Win(),
             _ => throw new ArgumentOutOfRangeException()
         };
 
         return newResponse;
     }
 
-    private ERockPaperScissors Win(ERockPaperScissors shape) => shape switch
-    {
-        ERockPaperScissors.Rock => ERockPaperScissors.Paper,
-        ERockPaperScissors.Paper => ERockPaperScissors.Scissors,
-        ERockPaperScissors.Scissors => ERockPaperScissors.Rock,
-        _ => throw new ArgumentOutOfRangeException(nameof(shape), shape, null)
-    };
-
-    private ERockPaperScissors Lose(ERockPaperScissors shape) => shape switch
-    {
-        ERockPaperScissors.Rock => ERockPaperScissors.Scissors,
-        ERockPaperScissors.Paper => ERockPaperScissors.Rock,
-        ERockPaperScissors.Scissors => ERockPaperScissors.Paper,
-        _ => throw new ArgumentOutOfRangeException(nameof(shape), shape, null)
-    };
-
     private ERockPaperScissors Parse(string c) =>
         c switch
         {
-            "A" => ERockPaperScissors.Rock,
-            "B" => ERockPaperScissors.Paper,
-            "C" => ERockPaperScissors.Scissors,
-            "X" => ERockPaperScissors.Rock,
-            "Y" => ERockPaperScissors.Paper,
-            "Z" => ERockPaperScissors.Scissors,
+            "A" or "X" => ERockPaperScissors.Rock,
+            "B" or "Y" => ERockPaperScissors.Paper,
+            "C" or "Z" => ERockPaperScissors.Scissors,
             _ => throw new ArgumentOutOfRangeException(nameof(c), c, null)
         };
 }

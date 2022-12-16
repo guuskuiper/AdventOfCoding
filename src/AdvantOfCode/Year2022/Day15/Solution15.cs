9680909d66e3474fa12e99e6e0af22494d2c8d54
Guus Kuiper <guus.kuiper@demcon.com>
2022-12-16 21:40:43 +0100

    Day15

--- src/AdvantOfCode/Year2022/Day15/Solution15.cs
+++ src/AdvantOfCode/Year2022/Day15/Solution15.cs
@@ -1,31 +1,22 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Drawing;
+using System.Numerics;
 
 namespace AdventOfCode.Year2022.Day15;
 
 public class Solution15 : Solution
 {
+    private const bool ExecuteParallel = true;
     private record Sensor(Point Location, Point Beacon);
     
     public string Run()
     {
-        string example = """ 
-Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-Sensor at x=9, y=16: closest beacon is at x=10, y=16
-Sensor at x=13, y=2: closest beacon is at x=15, y=3
-Sensor at x=12, y=14: closest beacon is at x=10, y=16
-Sensor at x=10, y=20: closest beacon is at x=10, y=16
-Sensor at x=14, y=17: closest beacon is at x=10, y=16
-Sensor at x=8, y=7: closest beacon is at x=2, y=10
-Sensor at x=2, y=0: closest beacon is at x=2, y=10
-Sensor at x=0, y=11: closest beacon is at x=2, y=10
-Sensor at x=20, y=14: closest beacon is at x=25, y=17
-Sensor at x=17, y=20: closest beacon is at x=21, y=22
-Sensor at x=16, y=7: closest beacon is at x=15, y=3
-Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3
-""";
+        var sw = Stopwatch.StartNew();
+
         var lines = InputReader.ReadFileLinesArray();
-        //var lines = example.Split("\r\n");
         List<Sensor> sensors = Parse(lines);
 
         int xMin = int.MaxValue;
@@ -39,8 +30,137 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3
         }
 
         int count = CountY(sensors, 2000000, xMin, xMax);
-        
-        return count + "\n";
+
+        List<(Sensor, Sensor)> pairs = new List<(Sensor, Sensor)>();
+        for (int i = 0; i < sensors.Count; i++)
+        {
+            Sensor first = sensors[i];
+            int firstBeaconDistance = Distance(first);
+            for (int j = i + 1; j < sensors.Count; j++)
+            {
+                Sensor second = sensors[j];
+                int secondBeaconDistance = Distance(second);
+                int sensorDistance = Distance(first.Location, second.Location);
+                if (sensorDistance < (firstBeaconDistance + secondBeaconDistance + 2))
+                {
+                    if (firstBeaconDistance < secondBeaconDistance)
+                    {
+                        pairs.Add((first, second));
+                    }
+                    else
+                    {
+                        pairs.Add((second, first));
+                    }
+                }
+            }
+        }
+
+        ConcurrentDictionary<Point, HashSet<Point>> boundaries = new();
+        ConcurrentDictionary<Point, int> intersections = new();
+
+        if (ExecuteParallel)
+        {
+            Parallel.ForEach(pairs, (sensorPair) =>
+            {
+                FindIntersections(sensorPair, boundaries, intersections);
+            });
+        }
+        else
+        {
+            foreach (var sensorPair in pairs)
+            {
+                FindIntersections(sensorPair, boundaries, intersections);
+            }
+        }
+
+        List<Point> validPoints = new();
+        foreach (var (point, value) in intersections.Where(x => x.Value > 1))
+        {
+            bool inside = false;
+            foreach (var sensor in sensors)
+            {
+                int distance = Distance(sensor);
+                int boundDistance = Distance(point, sensor.Location);
+                if (boundDistance <= distance)
+                {
+                    inside = true;
+                }
+            }
+
+            if (!inside)
+            {
+                validPoints.Add(point);
+            }
+        }
+
+        var target = validPoints[0];
+        BigInteger tuning = Tuning(target);
+
+        TimeSpan b = sw.Elapsed;
+        Console.WriteLine("Time: " + b);
+
+        return count + "\n" + tuning;
+    }
+
+    private void FindIntersections((Sensor, Sensor) sensorPair, ConcurrentDictionary<Point, HashSet<Point>> boundaries,
+        ConcurrentDictionary<Point, int> intersections)
+    {
+        var leftSensor = sensorPair.Item1;
+        var rightSensor = sensorPair.Item2;
+        HashSet<Point> leftBound;
+        if (!boundaries.ContainsKey(leftSensor.Location))
+        {
+            int distance = Distance(leftSensor);
+            int boundDistance = distance + 1;
+            leftBound = GetBoundInRange(leftSensor.Location, boundDistance, 0, 4000000);
+            boundaries[leftSensor.Location] = leftBound;
+        }
+        else
+        {
+            leftBound = boundaries[leftSensor.Location];
+        }
+
+        int distanceRight = Distance(rightSensor);
+        int rightBound = distanceRight + 1;
+        foreach (var point in leftBound)
+        {
+            if (Distance(point, rightSensor.Location) == rightBound)
+            {
+                intersections.AddOrUpdate(point, 1, (_, i) => i + 1);
+            }
+        }
+    }
+
+    private BigInteger Tuning(Point p) => p.X * new BigInteger(4000000) + p.Y;
+
+    private HashSet<Point> GetBoundInRange(Point p, int distance, int min, int max)
+    {
+        Size[] Offset =
+        {
+            new (1, 1), 
+            new (1, -1),
+            new (-1, -1),
+            new (-1, +1),
+        };
+
+        HashSet<Point> points = new HashSet<Point>();
+
+        Point current = p + new Size(-distance, 0);
+
+        foreach (var offset in Offset)
+        {
+            foreach (var _ in Enumerable.Range(0, distance))
+            {
+                current += offset;
+
+                if (current.X < min || current.Y < min) continue;
+                if (current.X > max || current.Y > max) continue;
+
+                points.Add(current);
+            }
+        }
+
+        return points;
     }
 
     private int CountY(List<Sensor> sensors, int yTarget, int xMin, int xMax)

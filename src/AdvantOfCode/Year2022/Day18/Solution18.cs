97e738fcbe4d58525e1bc3d13b04c45c0b420665
guuskuiper <guus.kuiper@demcon.nl>
2022-12-18 12:31:02 +0100

    Cleanup18 reuse BFS

--- src/AdvantOfCode/Year2022/Day18/Solution18.cs
+++ src/AdvantOfCode/Year2022/Day18/Solution18.cs
@@ -1,3 +1,5 @@
+using AdventOfCode.Graph;
+
 namespace AdventOfCode.Year2022.Day18;
 
 public class Solution18 : Solution
@@ -6,95 +8,40 @@ public class Solution18 : Solution
     
     public string Run()
     {
-        string example1 = "1,1,1\r\n2,1,1";
-        string example2 = """
-            2,2,2
-            1,2,2
-            3,2,2
-            2,1,2
-            2,3,2
-            2,2,1
-            2,2,3
-            2,2,4
-            2,2,6
-            1,2,5
-            3,2,5
-            2,1,5
-            2,3,5 
-            """;
-        //var lines = example2.Split("\r\n");
         var lines = InputReader.ReadFileLinesArray();
         var points = Parse(lines);
         var grid = BuildGrid(points);
-        var gridOutside = BuildOutsideGrid(grid);
-
         long count = Count(grid);
-        long countB = CountOutside(grid, gridOutside);
+        
+        var gridOutside = BuildOutsideGrid(grid);
+        var externalGrid = ExternalGrid(grid, gridOutside);
+        long countB = Count(externalGrid);
+        
         return count + "\n" + countB;
-    }
-
-    private bool[,,] BuildOutsideGrid(bool[,,] grid)
-    {
-        Queue<Point3> points = new();
-        HashSet<Point3> visited = new();
-        Point3 first = new Point3(0, 0, 0);
-        points.Enqueue(first);
-
-        while (points.Count > 0)
-        {
-            Point3 current = points.Dequeue();
-
-            foreach (Point3 neighbour in Neighbours(grid, current))
-            {
-                if (IsInside(grid, neighbour) && !IsFilled(grid, neighbour))
-                {
-                    if (!visited.Contains(neighbour))
-                    {
-                        points.Enqueue(neighbour);
-                        visited.Add(neighbour);
-                    }
-                }
-            }
-        }
-
-        bool[,,] outside = new bool[grid.GetLength(0), grid.GetLength(1), grid.GetLength(2)];
-        foreach (var p in visited)
-        {
-            SetGrid(outside, p, true);
-        }
-
-        return outside;
     }
 
     private long Count(bool[,,] grid)
     {
         long count = 0;
-        for (int x = 0; x < grid.GetLength(0); x++)
+        foreach (var p in GridPoints(grid))
         {
-            for (int y = 0; y < grid.GetLength(1); y++)
+            if(!IsFilled(grid, p)) continue;
+            
+            var neighbours = Neighbours(p);
+            foreach (Point3 neighbour in neighbours)
             {
-                for (int z = 0; z < grid.GetLength(2); z++)
+                if (!IsInside(grid, neighbour) || !IsFilled(grid, neighbour))
                 {
-                    Point3 p = new Point3(x, y, z);
-                    if(!IsFilled(grid, p)) continue;
-                    
-                    var neighbours = Neighbours(grid, p);
-                    foreach (Point3 neighbour in neighbours)
-                    {
-                        if (!IsInside(grid, neighbour) || !IsFilled(grid, neighbour))
-                        {
-                            count++;
-                        }
-                    }
+                    count++;
                 }
             }
         }
 
         return count;
     }
-    private long CountOutside(bool[,,] grid, bool[,,] outside)
+    
+    private IEnumerable<Point3> GridPoints(bool[,,] grid)
     {
-        long count = 0;
         for (int x = 0; x < grid.GetLength(0); x++)
         {
             for (int y = 0; y < grid.GetLength(1); y++)
@@ -102,25 +49,10 @@ public class Solution18 : Solution
                 for (int z = 0; z < grid.GetLength(2); z++)
                 {
                     Point3 p = new Point3(x, y, z);
-                    if(!IsFilled(grid, p)) continue;
-                    
-                    var neighbours = Neighbours(grid, p);
-                    foreach (Point3 neighbour in neighbours)
-                    {
-                        if (!IsInside(grid, neighbour))
-                        {
-                            count++;
-                        }
-                        else if (!IsFilled(grid, neighbour) && IsFilled(outside, neighbour))
-                        {
-                            count++;
-                        }
-                    }
+                    yield return p;
                 }
             }
         }
-
-        return count;
     }
 
     private bool IsInside(bool[,,] grid, Point3 point)
@@ -129,6 +61,7 @@ public class Solution18 : Solution
             point.Y >= 0 && point.Y < grid.GetLength(1) &&
             point.Z >= 0 && point.Z < grid.GetLength(2);
     }
+    
     private bool IsFilled(bool[,,] grid, Point3 point)
     {
         return grid[point.X, point.Y, point.Z];
@@ -140,7 +73,7 @@ public class Solution18 : Solution
     }
 
     private static readonly int[] Offsets = { -1, 1 };
-    private IEnumerable<Point3> Neighbours(bool[,,] grid, Point3 point)
+    private IEnumerable<Point3> Neighbours(Point3 point)
     {
         foreach (var dx in Offsets)
         {
@@ -161,19 +94,107 @@ public class Solution18 : Solution
         }
     }
 
-    private bool[,,] BuildGrid(List<Point3> point3s)
+    private bool[,,] BuildGrid(List<Point3> points)
     {
-        int maxX = point3s.Max(p => p.X);
-        int maxY = point3s.Max(p => p.Y);
-        int maxZ = point3s.Max(p => p.Z);
+        int maxX = points.Max(p => p.X);
+        int maxY = points.Max(p => p.Y);
+        int maxZ = points.Max(p => p.Z);
 
         bool[,,] grid = new bool[maxX + 1, maxY + 1, maxZ + 1];
-        foreach (Point3 point3 in point3s)
+        foreach (Point3 point3 in points)
         {
             grid[point3.X, point3.Y, point3.Z] = true;
         }
 
         return grid;
+    }
+
+    private class CubeGraph : IGraph<Point3>
+    {
+        private readonly bool[,,] _grid;
+        
+        public CubeGraph(bool[,,] grid)
+        {
+            _grid = grid;
+        }
+        
+        public IEnumerable<Point3> Neighbors(Point3 node)
+        {
+            foreach (Point3 neighbor in NeighborsCore(node))
+            {
+                if (IsInsideBounds(neighbor) && !IsCube(neighbor))
+                {
+                    yield return neighbor;
+                }
+            }
+        }
+        
+        private static readonly int[] Offsets = { -1, 1 };
+        private IEnumerable<Point3> NeighborsCore(Point3 point)
+        {
+            foreach (var dx in Offsets)
+            {
+                int x = point.X + dx;
+                yield return point with { X = x };
+            }
+
+            foreach (var dy in Offsets)
+            {
+                int y = point.Y + dy;
+                yield return point with { Y = y };
+            }
+
+            foreach (var dz in Offsets)
+            {
+                int z = point.Z + dz;
+                yield return point with { Z = z };
+            }
+        }
+        
+        private bool IsInsideBounds(Point3 point)
+        {
+            return point.X >= 0 && point.X < _grid.GetLength(0) &&
+                   point.Y >= 0 && point.Y < _grid.GetLength(1) &&
+                   point.Z >= 0 && point.Z < _grid.GetLength(2);
+        }
+        private bool IsCube(Point3 point)
+        {
+            return _grid[point.X, point.Y, point.Z];
+        }
+    }
+
+    private bool[,,] BuildOutsideGrid(bool[,,] grid)
+    {
+        CubeGraph graph = new CubeGraph(grid);
+        var parents = BFS.SearchFrom(graph, new Point3(0, 0, 0), null);
+        
+        bool[,,] outside = new bool[grid.GetLength(0), grid.GetLength(1), grid.GetLength(2)];
+        foreach (var p in parents.Keys)
+        {
+            SetGrid(outside, p, true);
+        }
+
+        return outside;
+    }
+    
+    private bool[,,] ExternalGrid(bool[,,] grid, bool[,,] outside)
+    {
+        bool[,,] result = new bool[grid.GetLength(0), grid.GetLength(1), grid.GetLength(2)];
+
+        for (int x = 0; x < result.GetLength(0); x++)
+        {
+            for (int y = 0; y < result.GetLength(1); y++)
+            {
+                for (int z = 0; z < result.GetLength(2); z++)
+                {
+                    bool isCube = grid[x, y, z];
+                    bool isOutside = outside[x, y, z];
+                    result[x, y, z] = isCube || !isOutside;
+                }
+            } 
+        }
+
+        return result;
     }
 
     private List<Point3> Parse(IEnumerable<string> lines)

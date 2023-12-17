061f14198906bf4b2ebf61567cee8e26f16713a0
guuskuiper <guus.kuiper@demcon.nl>
2023-12-17 11:49:37 +0100

    Day17

--- test/AdventOfCode.Test/Solutions/Year2023DaySolutionTest.cs
+++ test/AdventOfCode.Test/Solutions/Year2023DaySolutionTest.cs
@@ -23,4 +23,5 @@ public class Year2023DaySolutionTest : YearTests
     [Fact] public void Day14() => AssertDay("109385", "93102");
     [Fact] public void Day15() => AssertDay("521434", "248279");
     [Fact] public void Day16() => AssertDay("8551", "8754");
+    [Fact] public void Day17() => AssertDay("814", "974");
 }

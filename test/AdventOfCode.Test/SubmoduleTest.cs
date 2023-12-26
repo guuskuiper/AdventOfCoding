7fe0294dfc33ef970ccb89032ed9f26fba44ac28
guuskuiper <guus.kuiper@demcon.nl>
2023-12-26 10:51:32 +0100

    Add submodule test

---
+++ test/AdventOfCode.Test/SubmoduleTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using Xunit;
+
+namespace AdventOfCode.Test;
+
+public class SubmoduleTest
+{
+    [Fact]
+    public void InputReadmeExists()
+    {
+        string path = CallingFilePath();
+        string readmePath = Path.Combine(path, "..", "..", "input", "README.md");
+        string readmePathAbs = Path.GetFullPath(readmePath);
+        
+        bool exists = File.Exists(readmePathAbs);
+        
+        Assert.True(exists);
+    }
+
+    private string CallingFilePath([CallerFilePath] string path = "")
+    {
+        return Path.GetDirectoryName(path)!;
+    }
+}
